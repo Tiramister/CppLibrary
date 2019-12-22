ad9a288f2d2acbb0f7c3dd82838003b5bf9be8b1
Tiramister <nh111424@gmail.com>
2019-12-23 06:50:42 +0900

    add: Number/extgcd.cpp

---
+++ Number/extgcd.cpp
@@ -0,0 +1,24 @@
+#include <algorithm>
+
+template <typename T>
+T extgcd(T a, T b, T& x, T& y) {
+    T s = a, t = b,
+      xs = 1, ys = 0,
+      xt = 0, yt = 1;
+
+    while (t != 0) {
+        T div = s / t;
+
+        s -= t * div;
+        xs -= xt * div;
+        ys -= yt * div;
+
+        std::swap(s, t);
+        std::swap(xs, xt);
+        std::swap(ys, yt);
+    }
+
+    x = xs;
+    y = ys;
+    return s;
+}
