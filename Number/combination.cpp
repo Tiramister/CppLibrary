3c1e3b98f42a1917b5557baf05ef22a7b6ec34a7
Tiramister <nh111424@gmail.com>
2019-12-23 16:10:28 +0900

    add: combination

---
+++ Number/combination.cpp
@@ -0,0 +1,29 @@
+#include <vector>
+
+template <class T>
+struct Combination {
+    int max_n;
+    std::vector<T> f, invf;
+
+    explicit Combination(int n)
+        : max_n(n), f(n + 1), invf(n + 1) {
+        f[0] = 1;
+        for (int i = 1; i <= n; ++i) {
+            f[i] = f[i - 1] * i;
+        }
+
+        invf[max_n] = f[max_n].inv();
+        for (int i = max_n - 1; i >= 0; --i) {
+            invf[i] = invf[i + 1] * (i + 1);
+        }
+    }
+
+    T fact(int n) const { return f[n]; }
+    T invfact(int n) const { return invf[n]; }
+    T perm(int a, int b) const {
+        return a < b ? T(0) : f[a] * invf[a - b];
+    }
+    T comb(int a, int b) const {
+        return a < b ? T(0) : f[a] * invf[a - b] * invf[b];
+    }
+};
