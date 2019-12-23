68e81f6d44f98490a8e00c2b1c1ec3118b57192d
Tiramister <nh111424@gmail.com>
2019-12-23 10:15:38 +0900

    test: dijkstra's algorithm

---
+++ Graph/Verify/dijkstra.test.cpp
@@ -0,0 +1,33 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/5/GRL/1/GRL_1_A"
+
+#define __guard__
+#include "../../Misc/heap_alias.cpp"
+#include "../graph.cpp"
+#include "../dijkstra.cpp"
+#undef __guard__
+
+#include <iostream>
+
+constexpr int INF = std::numeric_limits<int>::max();
+
+int main() {
+    int n, m, s;
+    std::cin >> n >> m >> s;
+
+    Graph<int> graph(n);
+    while (m--) {
+        int u, v, d;
+        std::cin >> u >> v >> d;
+        graph[u].emplace_back(u, v, d);
+    }
+
+    auto dist = dijkstra(graph, s);
+    for (auto d : dist) {
+        if (d == INF) {
+            std::cout << "INF" << std::endl;
+        } else {
+            std::cout << d << std::endl;
+        }
+    }
+    return 0;
+}
