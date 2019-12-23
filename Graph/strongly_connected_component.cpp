79910709a573bcbe1e6213611faf998ae418a14e
Tiramister <nh111424@gmail.com>
2019-12-23 18:48:58 +0900

    add: strongly connected component

---
+++ Graph/strongly_connected_component.cpp
@@ -0,0 +1,58 @@
+#ifndef __guard__
+#define __guard__
+#include "graph.cpp"
+#undef __guard__
+#endif
+
+#include <algorithm>
+#include <vector>
+
+template <class Cost = int>
+struct StronglyConnectedComponents {
+    Graph<Cost> graph, rgraph;
+    std::vector<bool> visited;
+    std::vector<int> stk;
+
+    // id[v] = 頂点vはgroups[id[v]]に属する
+    std::vector<int> id;
+    std::vector<std::vector<int>> groups;
+
+    explicit StronglyConnectedComponents(const Graph<Cost>& g)
+        : graph(g), visited(graph.size(), false), id(graph.size(), -1) {
+        revinit();
+
+        for (int v = 0; v < graph.size(); ++v) dfs(v);
+
+        while (!stk.empty()) {
+            int v = stk.back();
+            stk.pop_back();
+            if (id[v] < 0) {
+                groups.push_back(std::vector<int>());
+                rdfs(v);
+            }
+        }
+    }
+
+    void revinit() {
+        rgraph = Graph<Cost>(graph.size());
+        for (int v = 0; v < graph.size(); ++v) {
+            for (const auto& e : graph[v]) {
+                rgraph[e.dst].emplace_back(e.dst, v, e.cost);
+            }
+        }
+    }
+
+    void dfs(int v) {
+        if (visited[v]) return;
+        visited[v] = true;
+        for (const auto& e : graph[v]) dfs(e.dst);
+        stk.push_back(v);
+    }
+
+    void rdfs(int v) {
+        if (id[v] >= 0) return;
+        id[v] = groups.size() - 1;
+        groups.back().push_back(v);
+        for (const auto& e : rgraph[v]) rdfs(e.dst);
+    }
+};
