d415fecab711e37b5e8506916a451d3b6df43ac8
Tiramister <nh111424@gmail.com>
2019-12-23 16:21:21 +0900

    change test: combination

--- Number/Verify/combination.test.cpp
+++ Number/Verify/combination.test.cpp
@@ -1,4 +1,4 @@
-#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/7/DPL/all/DPL_5_E"
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/7/DPL/all/DPL_5_C"
 
 #include "../modint.cpp"
 #include "../combination.cpp"
@@ -11,6 +11,11 @@ const Combination<mint> C(1000);
 int main() {
     int n, k;
     std::cin >> n >> k;
-    std::cout << C.comb(k, n) << std::endl;
+
+    mint ans = 0;
+    for (int i = 0; i <= k; ++i) {
+        ans += mint(-1).pow(k - i) * C.comb(k, i) * mint(i).pow(n);
+    }
+    std::cout << ans << std::endl;
     return 0;
 }
