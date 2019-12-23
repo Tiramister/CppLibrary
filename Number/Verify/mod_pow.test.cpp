c2103a2f5f38835b9200b2093b01c0c6243d9cef
Tiramister <nh111424@gmail.com>
2019-12-23 15:39:56 +0900

    test: power of ModInt

---
+++ Number/Verify/mod_pow.test.cpp
@@ -0,0 +1,13 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/6/NTL/1/NTL_1_B"
+
+#include "../modint.cpp"
+
+constexpr int MOD = 1e9 + 7;
+using mint = ModInt<MOD>;
+
+int main() {
+    int m, n;
+    std::cin >> m >> n;
+    std::cout << mint(m).pow(n) << std::endl;
+    return 0;
+}
