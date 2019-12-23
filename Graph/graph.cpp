153ffd96b36cea66b4bc1abb746e88757cfac0d9
Tiramister <nh111424@gmail.com>
2019-12-23 09:19:46 +0900

    add: base of Graph

---
+++ Graph/graph.cpp
@@ -0,0 +1,12 @@
+#include <vector>
+
+template <class Cost = int>
+struct Edge {
+    int src, dst;
+    Cost cost;
+    Edge(int src = -1, int dst = -1, Cost cost = 1)
+        : src(src), dst(dst), cost(cost){};
+};
+
+template <class Cost = int>
+using Graph = std::vector<std::vector<Edge<Cost>>>;
