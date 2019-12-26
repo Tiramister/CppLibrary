839db64dfd01d021f421ea32e7222610155e20df
Tiramister <nh111424@gmail.com>
2019-12-26 19:26:24 +0900

    add: coordinate compression

---
+++ Misc/compress.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+#include <map>
+
+template <class T>
+std::map<T, int> compress(std::vector<T>& v) {
+    std::sort(v.begin(), v.end());
+    v.erase(std::unique(v.begin(), v.end()), v.end());
+
+    std::map<T, int> rev;
+    for (int i = 0; i < v.size(); ++i) {
+        rev[v[i]] = i;
+    }
+    return rev;
+}
