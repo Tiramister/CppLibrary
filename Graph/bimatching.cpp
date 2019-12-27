541e8dc0bc36969255dc30b49bbe24f8d895bf83
Tiramister <nh111424@gmail.com>
2019-12-28 04:02:01 +0900

    add: maximum bipartite matching

---
+++ Graph/bimatching.cpp
@@ -0,0 +1,32 @@
+#ifndef __guard__
+#define __guard__
+#include "dinic.cpp"
+#undef __guard__
+#endif
+
+struct BiMatching {
+    MaxFlow<int, true> mf;
+    int n, m, s, g;
+
+    explicit BiMatching(int n, int m)
+        : mf(n + m + 2), n(n), m(m), s(n + m), g(n + m + 1) {
+        for (int u = 0; u < n; ++u) {
+            mf.span(s, enc(u, false), 1);
+        }
+        for (int v = 0; v < m; ++v) {
+            mf.span(enc(v, true), g, 1);
+        }
+    }
+
+    int enc(int v, bool side) {
+        return v + (side ? n : 0);
+    }
+
+    void span(int u, int v) {
+        mf.span(enc(u, false), enc(v, true), 1);
+    }
+
+    int exec() {
+        return mf.exec(s, g);
+    }
+};
