064c5044f64e639319fd3be30c2426b8a0f0cbb3
Tiramister <nh111424@gmail.com>
2019-12-28 03:34:32 +0900

    test: Dinic's algorithm

---
+++ Graph/Verify/dinic.test.cpp
@@ -0,0 +1,20 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/5/GRL/all/GRL_6_A"
+
+#include "../dinic.cpp"
+
+#include <iostream>
+
+int main() {
+    int n, m;
+    std::cin >> n >> m;
+
+    MaxFlow<int, true> mf(n);
+    while (m--) {
+        int u, v, c;
+        std::cin >> u >> v >> c;
+        mf.span(u, v, c);
+    }
+
+    std::cout << mf.exec(0, n - 1) << std::endl;
+    return 0;
+}
