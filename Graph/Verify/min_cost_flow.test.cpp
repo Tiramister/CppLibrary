628c272e4f691eedacb1af094fa7218f4bca22b0
Tiramister <nh111424@gmail.com>
2019-12-29 02:01:46 +0900

    test: minimum cost flow

---
+++ Graph/Verify/min_cost_flow.test.cpp
@@ -0,0 +1,18 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/5/GRL/all/GRL_6_B"
+
+#include "../min_cost_flow.cpp"
+#include <iostream>
+
+int main() {
+    int n, m, f;
+    std::cin >> n >> m >> f;
+
+    MinCostFlow<int, int> mcf(n);
+    while (m--) {
+        int u, v, c, d;
+        std::cin >> u >> v >> c >> d;
+        mcf.span(u, v, c, d);
+    }
+    std::cout << mcf.exec(0, n - 1, f) << std::endl;
+    return 0;
+}
