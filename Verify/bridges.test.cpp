1cd0e77c31e06b1b44f13d891a40c9d847ebdd63
Tiramister <nh111424@gmail.com>
2019-12-29 14:43:25 +0900

    test: bridges by lowlink

---
+++ Verify/bridges.test.cpp
@@ -0,0 +1,38 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/5/GRL/all/GRL_3_B"
+
+#define __guard__
+#include "../Graph/graph.cpp"
+#include "../Graph/lowlink.cpp"
+#undef __guard__
+
+#include <iostream>
+#include <algorithm>
+
+int main() {
+    int n, m;
+    std::cin >> n >> m;
+
+    Graph<> graph(n);
+    while (m--) {
+        int u, v;
+        std::cin >> u >> v;
+        graph[u].emplace_back(u, v);
+        graph[v].emplace_back(v, u);
+    }
+
+    Lowlink<> ll(graph);
+
+    std::vector<std::pair<int, int>> ans;
+    for (auto& e : ll.bridges) {
+        if (e.src > e.dst) {
+            std::swap(e.src, e.dst);
+        }
+        ans.emplace_back(e.src, e.dst);
+    }
+    std::sort(ans.begin(), ans.end());
+
+    for (const auto& p : ans) {
+        std::cout << p.first << ' ' << p.second << std::endl;
+    }
+    return 0;
+}
