48e0cce7497941eeb17d67ac57081f24b1d63402
Tiramister <nh111424@gmail.com>
2019-12-29 14:42:46 +0900

    add: Graph/lowlink.cpp

---
+++ Graph/lowlink.cpp
@@ -0,0 +1,45 @@
+#ifndef __guard__
+#define __guard__
+#include "graph.cpp"
+#undef __guard__
+#endif
+
+template <class Cost = int>
+struct Lowlink {
+    Graph<Cost> graph;
+    int time;
+    std::vector<int> order, low;
+
+    std::vector<int> artics;
+    std::vector<Edge<Cost>> bridges;
+
+    explicit Lowlink(const Graph<Cost>& graph)
+        : graph(graph), order(graph.size(), -1), low(graph.size(), graph.size()) {
+        time = 0;
+        for (int v = 0; v < (int)graph.size(); ++v) {
+            if (order[v] < 0) dfs(v, -1);
+        }
+    }
+
+    void dfs(int v, int r) {
+        order[v] = low[v] = time++;
+        int deg = 0;
+        bool is_artic = false;
+
+        for (auto e : graph[v]) {
+            if (order[e.dst] < 0) {
+                ++deg;
+                dfs(e.dst, e.src);
+                low[e.src] = std::min(low[e.src], low[e.dst]);
+
+                if (order[e.src] <= low[e.dst]) is_artic = true;
+                if (order[e.src] < low[e.dst]) bridges.push_back(e);
+            } else if (e.dst != r) {
+                low[e.src] = std::min(low[e.src], order[e.dst]);
+            }
+        }
+
+        if (r < 0) is_artic = (deg > 1);
+        if (is_artic) artics.push_back(v);
+    }
+};
