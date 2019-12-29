8bc645aff82518d1eee5f3dabaf8c8c62d70a2cb
Tiramister <nh111424@gmail.com>
2019-12-29 16:26:48 +0900

    add: Convolution/number_theoretical_transform.cpp

---
+++ Convolution/number_theoretical_transform.cpp
@@ -0,0 +1,110 @@
+#ifndef __guard__
+#define __guard__
+#include "../Number/modint.cpp"
+#undef __guard__
+#endif
+
+#include <vector>
+
+template <int MOD, int Root>
+struct NumberTheoreticalTransform {
+    using mint = ModInt<MOD>;
+    using mints = std::vector<mint>;
+
+    std::vector<mint> zetas;
+
+    explicit NumberTheoreticalTransform() {
+        int exp = MOD - 1;
+        while (true) {
+            mint zeta = mint(Root).pow(exp);
+            zetas.push_back(zeta);
+            if (exp % 2 != 0) break;
+            exp /= 2;
+        }
+    }
+
+    void bitrev(mints& f) const {
+        int n = f.size();
+
+        for (int i = 0; i < n; ++i) {
+            int ti = i, ni = 0;
+            for (int k = 0; (1 << k) < n; ++k) {
+                int b = (ti & 1);
+                ti >>= 1;
+                ni <<= 1;
+                ni += b;
+            }
+
+            if (i < ni) {
+                std::swap(f[i], f[ni]);
+            }
+        }
+    }
+
+    void udft(mints& f, bool isinv) const {
+        if (f.size() <= 1) return;
+
+        int l = 1;
+        int k = 1 << l;
+        int n = f.size();
+
+        while (k <= n) {
+            mint zeta = zetas[l];
+            if (isinv) zeta = zeta.inv();
+
+            for (int r = 0; r < n / k; ++r) {
+                mint zetapow = 1;
+
+                for (int j = 0; j < k / 2; ++j) {
+                    int b = r * k + j;
+                    mint t = zetapow * f[b + k / 2];
+
+                    f[b + k / 2] = f[b] - t;
+                    f[b] = f[b] + t;
+
+                    zetapow *= zeta;
+                }
+            }
+
+            ++l;
+            k <<= 1;
+        }
+    }
+
+    void dft(mints& f, bool isinv) const {
+        bitrev(f);
+        udft(f, isinv);
+    }
+
+    mints ntt(mints f, mints g) const {
+        int fdeg = f.size(),
+            gdeg = g.size();
+
+        int k = 0;
+        while ((1 << k) < fdeg + gdeg) {
+            ++k;
+        }
+
+        int n = (1 << k);
+        f.resize(n, mint(0));
+        g.resize(n, mint(0));
+
+        dft(f, false);
+        dft(g, false);
+
+        mints h(n);
+        for (int i = 0; i < n; ++i) {
+            h[i] = f[i] * g[i];
+        }
+
+        dft(h, true);
+        while (h.size() > fdeg + gdeg - 1) {
+            h.pop_back();
+        }
+        for (auto& x : h) x /= n;
+        return h;
+    }
+};
+
+// constexpr int MOD = 998244353;
+// using NTT = NumberTheoreticalTransform<MOD, 3>;
