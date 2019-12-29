0cfba390c3456ed6569005c407414775e301ffd5
Tiramister <nh111424@gmail.com>
2019-12-29 14:56:38 +0900

    add: Graph/topological_sort.cpp

---
+++ Graph/topological_sort.cpp
@@ -0,0 +1,29 @@
+#ifndef __guard__
+#define __guard__
+#include "graph.cpp"
+#undef __guard__
+#endif
+
+#include <algorithm>
+
+template <class Cost = int>
+struct TopologicalSort {
+    Graph<Cost> graph;
+    std::vector<bool> visited;
+    std::vector<int> order, id;
+
+    explicit TopologicalSort(const Graph<Cost>& graph)
+        : graph(graph), visited(graph.size(), false), id(graph.size()) {
+        for (int v = 0; v < (int)graph.size(); ++v) dfs(v);
+        std::reverse(order.begin(), order.end());
+
+        for (int i = 0; i < (int)graph.size(); ++i) id[order[i]] = i;
+    }
+
+    void dfs(int v) {
+        if (visited[v]) return;
+        visited[v] = true;
+        for (const auto& e : graph[v]) dfs(e.dst);
+        order.push_back(v);
+    }
+};
