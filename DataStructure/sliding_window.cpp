9521303b8f08a672346dbb84760027c18e9d4949
Tiramister <nh111424@gmail.com>
2019-12-29 14:15:40 +0900

    add: DataStructure/sliding_window.cpp

---
+++ DataStructure/sliding_window.cpp
@@ -0,0 +1,31 @@
+#include <deque>
+#include <functional>
+
+template <class T>
+struct SlidingWindow {
+    using Cmp = std::function<bool(T, T)>;
+
+    std::deque<std::pair<T, int>> window;
+    Cmp cmp;
+    int l, r;
+
+    explicit SlidingWindow(Cmp cmp)
+        : window(), cmp(cmp), l(0), r(0) {}
+
+    void push(T val) {
+        while (!window.empty() && cmp(val, window.back().first)) {
+            window.pop_back();
+        }
+        window.emplace_back(val, r++);
+    }
+
+    T get() {
+        return window.front().first;
+    }
+
+    void pop() {
+        if (window.front().second == l++) {
+            window.pop_front();
+        }
+    }
+};
