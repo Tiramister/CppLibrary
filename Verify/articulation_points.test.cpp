2b9dd8f255fa4a927ac47dfb1bd7c74390ece8fd
Tiramister <nh111424@gmail.com>
2019-12-29 14:43:13 +0900

    test: articulation points by lowlink

---
+++ Verify/articulation_points.test.cpp
@@ -0,0 +1,31 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/5/GRL/all/GRL_3_A"
+
+#define __guard__
+#include "../Graph/graph.cpp"
+#include "../Graph/lowlink.cpp"
+#undef __guard__
+
+#include <iostream>
+#include <algorithm>
+
+int main() {
+    int n, m;
+    std::cin >> n >> m;
+
+    Graph<> graph(n);
+    while (m--) {
+        int u, v;
+        std::cin >> u >> v;
+        graph[u].emplace_back(u, v);
+        graph[v].emplace_back(v, u);
+    }
+
+    Lowlink<> ll(graph);
+
+    auto& ans = ll.artics;
+    std::sort(ans.begin(), ans.end());
+    for (auto v : ans) {
+        std::cout << v << std::endl;
+    }
+    return 0;
+}
