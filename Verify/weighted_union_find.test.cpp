ac2a4b66cf4a6fbefccdc129349dbb742b57b697
Tiramister <nh111424@gmail.com>
2019-12-29 13:44:21 +0900

    test: weighted union find

---
+++ Verify/weighted_union_find.test.cpp
@@ -0,0 +1,31 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/3/DSL/1/DSL_1_B"
+
+#define __guard__
+#include "../DataStructure/weighted_union_find.cpp"
+#undef __guard__
+
+#include <iostream>
+
+int main() {
+    int n, q;
+    std::cin >> n >> q;
+    WeightedUnionFind<int> wuf(n);
+
+    while (q--) {
+        int t, u, v;
+        std::cin >> t >> u >> v;
+
+        if (t == 0) {
+            int d;
+            std::cin >> d;
+            wuf.unite(v, u, d);
+        } else {
+            if (!wuf.same(u, v)) {
+                std::cout << '?' << std::endl;
+            } else {
+                std::cout << wuf.diff(u, v) << std::endl;
+            }
+        }
+    }
+    return 0;
+}
