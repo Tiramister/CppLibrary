e2a7f44b9dcae0b3e368463cbb1aefbea126c869
Tiramister <nh111424@gmail.com>
2019-12-29 12:50:33 +0900

    add: Graph/kruskal.cpp

---
+++ Graph/kruskal.cpp
@@ -0,0 +1,21 @@
+#ifndef __guard__
+#define __guard__
+#include "graph.cpp"
+#include "../DataStructure/union_find.cpp"
+#undef __guard__
+#endif
+
+template <class Cost>
+Cost kruskal(int vnum, std::vector<Edge<Cost>>& edges) {
+    std::sort(edges.begin(), edges.end(),
+              [](const auto& lhs, const auto& rhs) { return lhs.cost < rhs.cost; });
+
+    UnionFind uf(vnum);
+    Cost sum = 0;
+    for (const auto& e : edges) {
+        if (uf.same(e.src, e.dst)) continue;
+        sum += e.cost;
+        uf.unite(e.src, e.dst);
+    }
+    return sum;
+}
