c219ffd79bbf13befa5ed3c73a7261ece6bfdb63
Tiramister <nh111424@gmail.com>
2020-04-02 22:11:18 +0900

    delete: debug functions

--- Debug/pair.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-template <class L, class R>
-std::ostream& operator<<(std::ostream& os, const std::pair<L, R>& p) {
-    return os << "(" << p.first << "," << p.second << ")";
-}
