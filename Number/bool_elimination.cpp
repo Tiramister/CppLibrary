e8d61ac9d894d83bbce06e8cb199cc841041c531
Tiramister <nh111424@gmail.com>
2020-04-02 22:05:02 +0900

    add: Gaussian elimination for boolean matrix

---
+++ Number/bool_elimination.cpp
@@ -0,0 +1,20 @@
+#include <algorithm>
+#include <vector>
+
+void bool_elimination(std::vector<std::vector<int>>& mat) {
+    int n = mat.size(), m = mat.front().size();
+
+    for (int l = 0; l < n; ++l) {
+        {
+            auto& v = *std::max_element(mat.begin() + l, mat.end());
+            std::swap(v, mat[l]);
+        }
+
+        const auto& v = mat[l];
+        for (int i = l + 1; i < n; ++i) {
+            auto u = mat[i];
+            for (int j = 0; j < m; ++j) u[j] ^= v[j];
+            if (u < mat[i]) std::swap(u, mat[i]);
+        }
+    }
+}
