958752fc8653435889101401e4e2f95585a644a9
Tiramister <nh111424@gmail.com>
2020-04-02 22:04:23 +0900

    add: Timer

---
+++ Misc/timer.cpp
@@ -0,0 +1,16 @@
+#include <chrono>
+
+struct Timer {
+    std::chrono::time_point<std::chrono::system_clock> begin;
+
+    void init() {
+        begin = std::chrono::system_clock::now();
+    }
+
+    int get_ms() {
+        auto cur = std::chrono::system_clock::now();
+        return std::chrono::
+            duration_cast<std::chrono::milliseconds>(cur - begin)
+                .count();
+    }
+};
