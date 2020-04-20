3dc2df427a54ebab23785783f9868e8be2cd92d4
Tiramister <nh111424@gmail.com>
2020-04-20 22:05:09 +0900

    add: Breadth First Search

---
+++ Graph/bfs.cpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "graph.cpp"
+
+#include <queue>
+
+template <class Cost>
+std::vector<int> bfs(const Graph<Cost>& graph, int s) {
+    std::vector<Cost> dist(graph.size(), -1);
+    dist[s] = 0;
+    std::queue<int> que;
+    que.push(s);
+
+    while (!que.empty()) {
+        int v = que.front();
+        que.pop();
+
+        for (const auto& e : graph[v]) {
+            if (dist[e.dst] != -1) continue;
+            dist[e.dst] = dist[v] + e.cost;
+            que.push(e.dst);
+        }
+    }
+
+    return dist;
+}
