a2bcf8d4715ec3d83bcf6810e8fcb715f168262d
Tiramister <nh111424@gmail.com>
2020-04-20 22:05:44 +0900

    add: Diameter of tree

---
+++ Graph/diameter.cpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "graph.cpp"
+#include "bfs.cpp"
+
+#include <algorithm>
+
+template <class Cost>
+std::pair<int, std::pair<int, int>> diameter(const Graph<Cost>& graph) {
+    int u, v;
+    {
+        auto dist = bfs(graph, 0);
+        u = std::distance(dist.begin(),
+                          std::max_element(dist.begin(), dist.end()));
+    }
+
+    auto dist = bfs(graph, u);
+    v = std::distance(dist.begin(),
+                      std::max_element(dist.begin(), dist.end()));
+
+    return std::make_pair(dist[v], std::make_pair(u, v));
+}
