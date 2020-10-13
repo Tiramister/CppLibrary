68206536329d89d146b7f8551072b147f63694c1
Tiramister <nh111424@gmail.com>
2020-10-13 21:34:07 +0900

    change: basic graph interface

--- Graph/graph.hpp
+++ Graph/graph.hpp
@@ -6,31 +6,21 @@ template <class Cost = int>
 struct Edge {
     int src, dst;
     Cost cost;
-    Edge(int src = -1, int dst = -1, Cost cost = 1)
+
+    Edge() = default;
+    Edge(int src, int dst, Cost cost = 1)
         : src(src), dst(dst), cost(cost){};
 
-    bool operator<(const Edge<Cost>& e) const { return this->cost < e.cost; }
-    bool operator>(const Edge<Cost>& e) const { return this->cost > e.cost; }
+    bool operator<(const Edge<Cost>& e) const { return cost < e.cost; }
+    bool operator>(const Edge<Cost>& e) const { return cost > e.cost; }
 };
 
 template <class Cost = int>
-using Edges = std::vector<Edge<Cost>>;
-
-template <class Cost = int>
-struct Graph {
-    std::vector<std::vector<Edge<Cost>>> graph;
-
-    Graph(int n = 0) : graph(n) {}
+struct Graph : public std::vector<std::vector<Edge<Cost>>> {
+    Graph(int n = 0) : std::vector<std::vector<Edge<Cost>>>(n) {}
 
     void span(bool direct, int src, int dst, Cost cost = 1) {
-        graph[src].emplace_back(src, dst, cost);
-        if (!direct) graph[dst].emplace_back(dst, src, cost);
+        (*this)[src].emplace_back(src, dst, cost);
+        if (!direct) (*this)[dst].emplace_back(dst, src, cost);
     }
-
-    int size() const { return graph.size(); }
-    void clear() { graph.clear(); }
-    void resize(int n) { graph.resize(n); }
-
-    std::vector<Edge<Cost>>& operator[](int v) { return graph[v]; }
-    std::vector<Edge<Cost>> operator[](int v) const { return graph[v]; }
 };
